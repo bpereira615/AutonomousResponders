7b7fe36a41ff32262bc0a93e08e00b72a5a48cd6
CalvinPollard <pollard@jhu.edu>
2017-05-05 01:29:12 -0400

    Fixed everything, shit now works(knock on wood)

--- Crane/Crane.ino
+++ Crane/Crane.ino
@@ -15,7 +15,9 @@
 #define BASE_COUNTER 13
 #define BASE_INTER 3
 
-
+//import Servo library
+#include <Servo.h>
+Servo claw_servo;
 
 volatile int spool_dist=0;
 volatile int base_dist=0;
@@ -23,9 +25,6 @@ volatile int base_dist=0;
 
 #include <Wire.h>  
 #include <PixyI2C.h>
-
-
-
 PixyI2C pixy;
 int xco;
 int yco;
@@ -52,16 +51,25 @@ void setup() {
   pixy.init();
 
   pinMode(CARRIAGE_MOTOR, OUTPUT);
+  digitalWrite(CARRIAGE_MOTOR, LOW);
   pinMode(CARRIAGE_FORWARD, OUTPUT);
+  digitalWrite(CARRIAGE_FORWARD, LOW);
   pinMode(CARRIAGE_BACKWARD, OUTPUT);
+  digitalWrite(CARRIAGE_BACKWARD, LOW);
 
   pinMode(SPOOL, OUTPUT);//DC motor on/off
+  digitalWrite(SPOOL, LOW);
   pinMode(SPOOL_UP, OUTPUT);//DC direction 1
+  digitalWrite(SPOOL_UP, LOW);
   pinMode(SPOOL_DOWN, OUTPUT);//DC direction 2
+  digitalWrite(SPOOL_DOWN, LOW);
   
   pinMode(BASE, OUTPUT);//DC motor on/off
+  digitalWrite(BASE, LOW);
   pinMode(BASE_CLOCK, OUTPUT);//DC direction 1
+  digitalWrite(BASE_CLOCK, LOW);
   pinMode(BASE_COUNTER, OUTPUT);//DC direction 2 
+  digitalWrite(BASE_COUNTER, LOW);
   
   pinMode(SPOOL_INTER, INPUT);
   pinMode(BASE_INTER, INPUT);
@@ -75,18 +83,23 @@ void setup() {
   
   analogWrite(CARRIAGE_MOTOR, 0);
 
-  delay(2000);//lets pixy find object
-}
+  claw_servo.attach(CLAW_SERVO);
+  close_claw();
 
-void SpoolDistance(){//handles interrupt from right wheel motor encoder
-  spool_dist++;
-}
-
-void BaseDistance(){////handles interrupt from left wheel motor encoder
-  base_dist++;
+  
 }
 
 void loop(){
+  delay(5000);
+  /*
+  delay(5000);
+  refresh(1);
+  delay(2000);
+  findObjectTH(1);
+//  refresh(1);
+  Serial.println(xco);
+  delay(10000000);
+  */
   /*
   digitalWrite(SPOOL_UP, HIGH);
   digitalWrite(SPOOL_DOWN, LOW);
@@ -116,20 +129,62 @@ void loop(){
   delay(3000); 
 */
 
-  
-  refresh();
-//  Serial.println(xco);
-  Serial.println(yco);
-//  findObject();
+ 
+  //ACTUAL CONTROL FLOW
+  refresh(1);
+  for(int i; i<10;i++){//45 degrees
+    ccw();
+  }
+  delay(2000);//lets pixy find object
+  findObjectTH(1);
+  findObjectR(1);
+
+  digitalWrite(SPOOL_UP,HIGH);
+  digitalWrite(SPOOL_DOWN,LOW);
+  digitalWrite(SPOOL, HIGH);
   delay(1000);
+  digitalWrite(SPOOL, LOW);
   
+  open_claw();
+  claw_down(1600);
+  delay(1500);
+  close_claw();
+  delay(1500);
+  digitalWrite(SPOOL_UP,HIGH);
+  digitalWrite(SPOOL_DOWN,LOW);
+  digitalWrite(SPOOL, HIGH);
+  delay(2500);
+  digitalWrite(SPOOL, LOW);
+  for(int i; i<20;i++){//90 degrees
+    ccw();
+  }
+  refresh(2);
+  delay(2000);
+  findObjectTH(2);
+  findObjectR(2);
+
+  digitalWrite(SPOOL_UP,HIGH);
+  digitalWrite(SPOOL_DOWN,LOW);
+  digitalWrite(SPOOL, HIGH);
+  delay(1000);
+  digitalWrite(SPOOL, LOW);
+
+  claw_down(4850);
+  
+  delay(1500);
+  open_claw();
+  delay(1500);
+  claw_up(4600);
+
+  Serial.print('a');
+  delay(10000000);
 }
 
-void refresh(){
+void refresh(int code){//updates x and y coordinates of selected color code
   block=pixy.getBlocks();
   if(block){
     for(int i;i<block;i++){
-      if(pixy.blocks[i].signature==1){
+      if(pixy.blocks[i].signature==code){
         xco=pixy.blocks[i].x;
         yco=pixy.blocks[i].y;
       }
@@ -137,22 +192,34 @@ void refresh(){
   }
 }
 
-void findObject(){
+void findObjectR(int code){//finds color code in R direction
   boolean found = false;
   while(!found){
-    refresh();
-    /*
-    if(xco>180){
-      ccw();
-    }else if(xco<140){
-      cw();
-    }else 
-    */
-    if(yco<80){
+    refresh(code);
+    if(yco<90){
       forward();
-    }else if(yco>120){
+    }else if(yco>110){
       back();
     }else{
+      found=true;
+    }
+  }
+  digitalWrite(CARRIAGE_FORWARD, HIGH);
+  digitalWrite(CARRIAGE_BACKWARD, LOW);
+  digitalWrite(CARRIAGE_MOTOR, HIGH);
+  delay(50);
+  digitalWrite(CARRIAGE_MOTOR, LOW);
+}
+
+void findObjectTH(int code){//finds color code in Theta direction
+  boolean found = false;
+  while(!found){
+    refresh(code);
+    if(xco>170){//pixy is installed backwards
+      ccw();
+    }else if(xco<150){
+      cw();
+    }else{ 
       found=true;
     }
   }
@@ -163,9 +230,13 @@ void forward(){
   digitalWrite(CARRIAGE_BACKWARD, LOW);
   Serial.print("Crane claw forward: ");
   Serial.println(yco);
+  digitalWrite(SPOOL_UP,HIGH);
+  digitalWrite(SPOOL_DOWN,LOW);
+  digitalWrite(SPOOL, HIGH);
   analogWrite(CARRIAGE_MOTOR, 255); //TODO: calibration
   delay(unit_time);
   digitalWrite(CARRIAGE_MOTOR, LOW);
+  digitalWrite(SPOOL, LOW);
   return;
 }
 
@@ -180,68 +251,81 @@ void back(){
   return;
 }
 
-void ccw(){
+void cw(){
   Serial.println("Crane boom counterclockwise!");
-  base_counterclockwise(unit_encoder);
-  
+  base_dist=0;//rezeros distance counter to avoid issues with int rollover
+  digitalWrite(BASE_CLOCK, LOW);
+  digitalWrite(BASE_COUNTER, HIGH);
+  while(base_dist< 25){
+    digitalWrite(BASE, HIGH);
+    delay(15);
+    digitalWrite(BASE, LOW);
+    delay(100);
+  }
+  digitalWrite(BASE, LOW);
+  return;
   return;
 }
 
-void cw(){
+void ccw(){
   Serial.println("Crane boom clockwise!");
-  base_clockwise(unit_encoder);
+  base_dist=0;//rezeros distance counter to avoid issues with int rollover
+  digitalWrite(BASE_CLOCK, HIGH);
+  digitalWrite(BASE_COUNTER, LOW);
+  digitalWrite(BASE, HIGH);
+  while(base_dist< 25){
+    digitalWrite(BASE, HIGH);
+    delay(15);
+    digitalWrite(BASE, LOW);
+    delay(100);
+  }
+  digitalWrite(BASE, LOW);
+  return;
   return;
 }
 
 
 void claw_down(int dist){//forward method, takes number of shaft revolutions
   spool_dist=0;//rezeros distance counter to avoid issues with int rollover
-  
   digitalWrite(SPOOL_UP,LOW);
-  while(spool_dist< dist){
-    digitalWrite(SPOOL_DOWN,HIGH);
-    delay(speed_control);
+  digitalWrite(SPOOL_DOWN,HIGH);
+  digitalWrite(SPOOL, HIGH);
+  while(spool_dist < dist){
+    //delay(speed_control);
   }
+  digitalWrite(SPOOL_UP,HIGH);
   digitalWrite(SPOOL_DOWN,LOW);
+  digitalWrite(SPOOL, HIGH);
+  delay(40);
+  digitalWrite(SPOOL, LOW);
   return;
 }
 
 
 void claw_up(int dist){//forward method, takes number of shaft revolutions
   spool_dist=0;//rezeros distance counter to avoid issues with int rollover
-  
+  digitalWrite(SPOOL_UP,HIGH);
   digitalWrite(SPOOL_DOWN,LOW);
+  digitalWrite(SPOOL, HIGH);
   while(spool_dist< dist){
-    digitalWrite(SPOOL_UP,HIGH);
-    delay(speed_control);
+    //delay(speed_control);
   }
-  digitalWrite(SPOOL_UP,LOW);
+  digitalWrite(SPOOL, LOW);
   return;
 }
 
-
-void base_clockwise(int dist){//forward method, takes number of shaft revolutions
-  base_dist=0;//rezeros distance counter to avoid issues with int rollover
-  
-  digitalWrite(BASE_COUNTER,LOW);
-  while(spool_dist< dist){
-    analogWrite(BASE_CLOCK, base_speed);
-    delay(speed_control);
-  }
-  analogWrite(BASE_CLOCK, 0);
-  return;
+void open_claw() {
+  claw_servo.write(0);
 }
 
+void close_claw() {
+  claw_servo.write(180);
+}
 
+void SpoolDistance(){//handles interrupt from right wheel motor encoder
+  spool_dist++;
+}
 
-void base_counterclockwise(int dist){//forward method, takes number of shaft revolutions
-  base_dist=0;//rezeros distance counter to avoid issues with int rollover
-  
-  digitalWrite(BASE_CLOCK,LOW);
-  while(spool_dist< dist){
-    analogWrite(BASE_COUNTER, base_speed);
-    delay(speed_control);
-  }
-  analogWrite(BASE_COUNTER, 0);
-  return;
+void BaseDistance(){////handles interrupt from left wheel motor encoder
+  base_dist++;
 }
